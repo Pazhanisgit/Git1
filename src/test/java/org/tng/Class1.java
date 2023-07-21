75aaa1f0a05528437106a6ce97d8b480de52b693
Pazhanisgit <smpazhanivel@gmail.com>
2023-07-21 23:09:17 +0530

    sample git

---
+++ src/test/java/org/tng/Class1.java
@@ -0,0 +1,20 @@
+package org.tng;
+
+import org.testng.annotations.Test;
+
+public class Class1 {
+	@Test
+	private void tc_1() {
+		System.out.println("class1 tc1");
+	}
+	
+	@Test
+	private void tc_2() {
+		System.out.println("class1 tc2");
+	}
+	@Test
+	private void tc_3() {
+		System.out.println("class1 tc3");
+	}
+	
+}
---
+++ src/test/java/org/tng/Class2.java
@@ -0,0 +1,20 @@
+package org.tng;
+
+import org.testng.annotations.Test;
+
+public class Class2 {
+	@Test
+	private void tc_1() {
+		System.out.println("class1 tc1");
+	}
+	
+	@Test
+	private void tc_2() {
+		System.out.println("class1 tc2");
+	}
+	@Test
+	private void tc_3() {
+		System.out.println("class1 tc3");
+	}
+
+}
